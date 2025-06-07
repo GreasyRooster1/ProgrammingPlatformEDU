9f78558d9e2ed4dfb698a2c361be81381190252f
GreasyRooster <greasyRooster1@gmail.com>
2025-06-07 14:32:01 -0700

    resizable panels

--- src/Pages/Editor/Editor.jsx
+++ src/Pages/Editor/Editor.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import ScreenPage from "../../components/screenPage/ScreenPage.jsx";
+import {Panel, PanelGroup, PanelResizeHandle} from "react-resizable-panels";
+import styles from "./editor.module.css";
 
 function Editor() {
     return (
         <ScreenPage>
-            editor
+            <PanelGroup autoSaveId="example" storage={null} direction="horizontal">
+                <Panel defaultSize={25} className={styles.stepsPanel}>
+                    steps
+                </Panel>
+                <PanelResizeHandle />
+
+                <Panel className={styles.codePanel}>
+                    code
+                </Panel>
+                <PanelResizeHandle />
+
+                <Panel defaultSize={25}>
+                    <PanelGroup autoSaveId="example" storage={null} direction="vertical">
+                        <Panel defaultSize={60} className={styles.outputPanel}>
+                            output
+                        </Panel>
+                        <PanelResizeHandle />
+                        <Panel defaultSize={60} className={styles.consolePanel}>
+                            console
+                        </Panel>
+                    </PanelGroup>
+                </Panel>
+            </PanelGroup>;
         </ScreenPage>
     );
 }
