24400ac63c0c9eb110c193c15a478946c8a619b1
GreasyRooster <greasyRooster1@gmail.com>
2025-06-07 14:19:50 -0700

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/editor" element={<About />} />
+        <Route path="/landing" element={<Contact />} />
+        {/* Add more routes as needed */}
+      </Routes>
   )
 }
 
