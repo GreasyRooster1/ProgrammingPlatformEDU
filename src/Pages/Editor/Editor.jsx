caf4484c7409b90991a87ba9abe5ff9cdbabfdca
GreasyRooster <greasyRooster1@gmail.com>
2025-06-07 15:11:05 -0700

    adjust margins

--- src/Pages/Editor/Editor.jsx
+++ src/Pages/Editor/Editor.jsx
@@ -13,6 +13,18 @@ function Editor() {
                     <Step title="Step 1" image={logo} type="info">
                         This is a test step
                     </Step>
+                    <Step title="Step 1" image={logo} type="code">
+                        This is a test step
+                    </Step>
+                    <Step title="Step 1" image={logo} type="challenge">
+                        This is a test step
+                    </Step>
+                    <Step title="Step 1" image={logo} type="check">
+                        This is a test step
+                    </Step>
+                    <Step title="Step 1" image={logo} type="next">
+                        This is a test step
+                    </Step>
                 </Panel>
                 <PanelResizeHandle />
 
