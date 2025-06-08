633e0deba75b34f467b5b7d1c2858e2eef5c240b
GreasyRooster <greasyRooster1@gmail.com>
2025-06-08 10:30:00 -0700

    numbers

--- src/Pages/Editor/panels/lesson/LessonPanel.jsx
+++ src/Pages/Editor/panels/lesson/LessonPanel.jsx
@@ -8,16 +8,16 @@ function LessonPanel() {
             <Step title="Step 1" image={logo} type="info">
                 This is a test step
             </Step>
-            <Step title="Step 1" image={logo} type="code">
+            <Step title="Step 2" image={logo} type="code">
                 This is a test step
             </Step>
-            <Step title="Step 1" image={logo} type="challenge">
+            <Step title="Step 3" image={logo} type="challenge">
                 This is a test step
             </Step>
-            <Step title="Step 1" image={logo} type="check">
+            <Step title="Step 4" image={logo} type="check">
                 This is a test step
             </Step>
-            <Step title="Step 1" image={logo} type="next">
+            <Step title="Step 5" image={logo} type="next">
                 This is a test step
             </Step>
         </>
