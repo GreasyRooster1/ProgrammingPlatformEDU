1161c26afefa4307ba808f88793c07919dea88ea
GreasyRooster <greasyRooster1@gmail.com>
2025-06-08 19:57:46 -0700

    example iframe

--- src/Pages/Editor/panels/output/OutputPanel.jsx
+++ src/Pages/Editor/panels/output/OutputPanel.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import HeaderFlex from "../../../../components/headerFlex/HeaderFlex.jsx";
 import styles from "./outputPanel.module.css";
 
+import play from "/icons/Play.svg"
+import stop from "/icons/Stop.svg"
+import link from "/icons/ExternalLink.svg"
+
 function OutputPanel() {
     return (
-        <HeaderFlex headClassName={styles.head} bodyClassName={styles.body} headContent={
+        <HeaderFlex className={styles.wrapper} headClassName={styles.head} bodyClassName={styles.body} headContent={
             <>
-                <div className={styles.controlButtons}>
-
+                <div className={`${styles.controlButtons} ${styles.iconContainers}`}>
+                    <img src={play}/>
+                    <img src={stop}/>
                 </div>
-                <div className={styles.popButtons}>
-
+                <div className={`${styles.popButtons} ${styles.iconContainers}`}>
+                    <img src={link}/>
                 </div>
             </>
         }>
-
+            <iframe className={styles.frame} src="https://example.com"></iframe>
         </HeaderFlex>
     );
 }
