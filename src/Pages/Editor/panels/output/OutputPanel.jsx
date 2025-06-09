731ce41edc3de3b95146e9e346b63d478650cdda
GreasyRooster <greasyRooster1@gmail.com>
2025-06-08 19:28:07 -0700

    output panel stuff

--- src/Pages/Editor/panels/output/OutputPanel.jsx
+++ src/Pages/Editor/panels/output/OutputPanel.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import HeaderFlex from "../../../../components/headerFlex/HeaderFlex.jsx";
+import styles from "./outputPanel.module.css";
 
 function OutputPanel() {
     return (
-        <HeaderFlex>
+        <HeaderFlex headClassName={styles.head} bodyClassName={styles.body} headContent={
+            <>
+                <div className={styles.controlButtons}>
+
+                </div>
+                <div className={styles.popButtons}>
+
+                </div>
+            </>
+        }>
 
         </HeaderFlex>
     );
