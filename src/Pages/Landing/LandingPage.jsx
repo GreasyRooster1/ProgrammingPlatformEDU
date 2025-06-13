087bd839e8cc75ae27266d736bafcdd6787a2afd
GreasyRooster <greasyRooster1@gmail.com>
2025-06-13 15:02:24 -0700

    logging

--- src/Pages/Landing/LandingPage.jsx
+++ src/Pages/Landing/LandingPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {useAuth0} from "@auth0/auth0-react";
 
 function LandingPage() {
+    const { loginWithRedirect,logout, user, isAuthenticated,isLoading} = useAuth0();
+
+    if(!isLoading && isAuthenticated) {
+        console.log(user)
+        console.log(user.user_id)
+    }
+
     return (
         <div>
-            ladning
+            landing
+            <button onClick={() => loginWithRedirect()}>Login</button>
+            <button onClick={() => logout({logoutParams: {returnTo: window.location.origin}})}>
+                Log Out
+            </button>
         </div>
     );
 }
