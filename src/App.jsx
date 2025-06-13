bd2187b894e27dca8776e098e22aad09164eadcd
GreasyRooster <greasyRooster1@gmail.com>
2025-06-13 10:52:14 -0700

    login page route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import HomePage from "./Pages/Home/HomePage.jsx";
 import EditorPage from "./Pages/Editor/EditorPage.jsx";
 import LandingPage from "./Pages/Landing/LandingPage.jsx";
+import LoginPage from "./Pages/Login/LoginPage.jsx";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/editor" element={<EditorPage />} />
         <Route path="/landing" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
         {/* Add more routes as needed */}
       </Routes>
   )
