3f990b8d8c56619b043934db7e2163365456852f
GreasyRooster <greasyRooster1@gmail.com>
2025-06-13 10:49:15 -0700

    auth0 setup

---
+++ src/Pages/Login/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            login
+        </div>
+    );
+}
+
+export default HomePage;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom";
+import {Auth0Provider} from "@auth0/auth0-react";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+      <Auth0Provider
+          domain="dev-pqbjxkbpmw3pn3x7.us.auth0.com"
+          clientId="ZDY58e8OF70FyubRaf6r7VtoGaVNanNV"
+          authorizationParams={{
+              redirect_uri: window.location.origin
+          }}
+      >
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </Auth0Provider>
   </StrictMode>,
 )
