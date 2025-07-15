798c27897a9621430d6a2b28abc8528655a20b60
GreasyRooster <greasyRooster1@gmail.com>
2025-07-15 15:48:57 -0700

    working auth provider

--- src/Pages/Landing/LandingPage.jsx
+++ src/Pages/Landing/LandingPage.jsx
@@ -1,26 +1,23 @@
 import React, {useEffect, useState} from 'react';
 import {useAuth0} from "@auth0/auth0-react";
 import {getUserId} from "../../api/api.js";
+import AuthProvider, {defaultAuthData} from "../../components/AuthProvider.jsx";
 
 function LandingPage() {
-    const { loginWithRedirect,logout, user, isAuthenticated,isLoading,getAccessTokenSilently } = useAuth0();
-
-    if(!isLoading && isAuthenticated) {
-        console.log(user)
-        console.log(getUserId(user))
-        getAccessTokenSilently().then((e)=>{
-            console.log(e)
-        })
-    }
+    const {loginWithRedirect, logout } = useAuth0();
+    const [userData, setUserData] = useState(defaultAuthData());
 
     return (
-        <div>
-            landing
-            <button onClick={() => loginWithRedirect()}>Login</button>
-            <button onClick={() => logout({logoutParams: {returnTo: window.location.origin}})}>
-                Log Out
-            </button>
-        </div>
+        <AuthProvider setUserData={setUserData}>
+            <div>
+                landing
+                <button onClick={() => loginWithRedirect()}>Login</button>
+                <button onClick={() => logout({logoutParams: {returnTo: window.location.origin}})}>
+                    Log Out
+                </button>
+                token: {userData.token}
+            </div>
+        </AuthProvider>
     )
 }
 
---
+++ src/components/AuthProvider.jsx
@@ -0,0 +1,37 @@
+import React, {useEffect} from 'react';
+import {useAuth0} from "@auth0/auth0-react";
+import {getUserId} from "../api/api.js";
+
+function AuthProvider(props) {
+    const {user, isAuthenticated,isLoading,getAccessTokenSilently } = useAuth0();
+
+    useEffect(() => {
+        if(!isLoading && isAuthenticated) {
+            console.log(user)
+            let uid = getUserId(user);
+            getAccessTokenSilently().then((e)=>{
+                props.setUserData({
+                    token:e,
+                    uid:uid,
+                    isAuthenticated:true,
+                })
+            })
+        }
+    },[isLoading,isAuthenticated])
+
+    return (
+        <>
+            {props.children}
+        </>
+    );
+}
+
+export function defaultAuthData(){
+    return {
+        token:"",
+        uid:"",
+        isAuthenticated: false,
+    }
+}
+
+export default AuthProvider;
