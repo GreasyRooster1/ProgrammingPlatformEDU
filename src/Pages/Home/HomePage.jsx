f535e844cc8c09d793dd21d6ba7dfef3abe89ad0
GreasyRooster <greasyRooster1@gmail.com>
2025-07-15 16:07:20 -0700

    lock home to auth

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import AuthLock from "../../components/AuthLock.jsx";
 
 function HomePage() {
     return (
-        <div>
+        <AuthLock>
             home
-        </div>
+        </AuthLock>
     );
 }
 
