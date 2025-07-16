c85a2672eedd960053c21f3a8bea3f6c209ac0dc
GreasyRooster <greasyRooster1@gmail.com>
2025-07-16 16:36:39 -0700

    swap home and landing

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import AuthLock from "../../components/AuthLock.jsx";
+import styles from "./homePage.module.css";
+import {useAuth0} from "@auth0/auth0-react";
 
 function HomePage() {
+    let {user} = useAuth0();
     return (
         <AuthLock>
+            <div className={styles.header}>
+                <span className={styles.welcome}>Welcome, {user.name}</span>
+            </div>
             home
         </AuthLock>
     );
--- src/Pages/Landing/LandingPage.jsx
+++ src/Pages/Landing/LandingPage.jsx
@@ -1,17 +1,13 @@
 import React, {useState} from 'react';
 import {useAuth0} from "@auth0/auth0-react";
 import AuthProvider, {defaultAuthData} from "../../components/AuthProvider.jsx";
-import styles from "./landingPage.module.css"
 
 function LandingPage() {
-    const {loginWithRedirect, logout } = useAuth0();
+    const {loginWithRedirect, logout} = useAuth0();
     const [userData, setUserData] = useState(defaultAuthData());
 
     return (
         <AuthProvider setUserData={setUserData}>
-            <div className={styles.header}>
-                <span className={styles.welcome}>Welcome, </span>
-            </div>
             <div>
                 landing
                 <button onClick={() => loginWithRedirect()}>Login</button>
