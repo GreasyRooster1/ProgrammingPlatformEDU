e0de51ae73aff2928bf2252da0c13849b11c11fa
GreasyRooster <greasyRooster1@gmail.com>
2025-07-31 05:38:41 -0700

    remove all uses of uid

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -1,20 +1,16 @@
 import React, {useEffect} from 'react';
 import {useAuth0} from "@auth0/auth0-react";
-import {getUserId} from "../api/api.js";
 
 function AuthProvider(props) {
-    const {user, isAuthenticated,isLoading,getAccessTokenSilently } = useAuth0();
+    const {isAuthenticated,isLoading,getAccessTokenSilently } = useAuth0();
     let setUserData = props.setUserData;
     useEffect(() => {
         if(!isLoading && isAuthenticated) {
-            let uid = getUserId(user);
             getAccessTokenSilently().then((e)=>{
                 setUserData({
                     token:e,
-                    uid:uid,
                     isAuthenticated:true,
                 })
-                console.log(uid)
             })
         }
     },[isLoading,isAuthenticated])
@@ -25,7 +21,6 @@ function AuthProvider(props) {
 export function defaultAuthData(){
     return {
         token:"",
-        uid:"",
         isAuthenticated: false,
     }
 }
