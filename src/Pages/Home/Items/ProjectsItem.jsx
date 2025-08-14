6f33d9e595e5e7e8baa9e20229a5aa11649995e4
GreasyRooster <greasyRooster1@gmail.com>
2025-08-13 20:58:48 -0700

    projects item

---
+++ src/Pages/Home/Items/ProjectsItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './projectsItem.module.css'
+
+function ProjectsItem() {
+    return (
+        <>
+            <div className={styles.list}>
+
+            </div>
+            <div className={styles.details}>
+
+            </div>
+        </>
+    );
+}
+
+export default ProjectsItem;
