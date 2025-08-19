db56ba77537b2a1decba776cb144733c981ac7e5
GreasyRooster <greasyRooster1@gmail.com>
2025-08-18 19:06:11 -0700

    use auth

--- src/api/netutils.js
+++ src/api/netutils.js
@@ -1,22 +1,23 @@
 import {API_URL} from "./net.js";
 
 //quickly build a request handler for the backend
-function req(uri, method, func){
+function req(uri, method, useAuth, func){
     //args for the request
     return async (...args) => {
-        let argsUri = "/"+args.join("/");
+        let authHead = args[0];
+        let argsUri = "/"+args.slice(1).join("/");
         const response = await fetch(API_URL + uri + argsUri,{
             method: method,
             headers: {
-                "Authorization":
+                "Authorization":authHead
             }
         })
         return func.apply(this, [response,...args]);
     };
 }
 
-function jsonReq(uri){
-    return req(uri,"GET",(response)=>{
+function jsonReq(uri,useAuth){
+    return req(uri,"GET", useAuth, (response)=>{
         return response.json();
     })
 }
