21c82b43d0605ddc8338ab9a425a4e2d4eafe83d
GreasyRooster <greasyRooster1@gmail.com>
2025-08-18 18:26:39 -0700

    ataTE

--- src/api/netutils.js
+++ src/api/netutils.js
@@ -5,13 +5,18 @@ function req(uri, method, func){
     //args for the request
     return async (...args) => {
         let argsUri = "/"+args.join("/");
-        const response = await fetch(API_URL + uri + argsUri)
+        const response = await fetch(API_URL + uri + argsUri,{
+            method: method,
+            headers: {
+                "Authorization":
+            }
+        })
         return func.apply(this, [response,...args]);
     };
 }
 
 function jsonReq(uri){
-    return req(uri,(response)=>{
+    return req(uri,"GET",(response)=>{
         return response.json();
     })
 }
