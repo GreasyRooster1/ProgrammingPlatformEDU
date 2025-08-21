ca99606462c20f66b12c13851c62b9b3188a3fdf
GreasyRooster <greasyRooster1@gmail.com>
2025-08-20 18:22:42 -0700

    use auth works

--- src/api/netutils.js
+++ src/api/netutils.js
@@ -4,7 +4,7 @@ import {API_URL} from "./net.js";
 function req(uri, method, useAuth, func){
     //args for the request
     return async (...args) => {
-        let authHead = args[0];
+        let authHead = useAuth?args[0]:"";
         let argsUri = "/"+args.slice(1).join("/");
         const response = await fetch(API_URL + uri + argsUri,{
             method: method,
