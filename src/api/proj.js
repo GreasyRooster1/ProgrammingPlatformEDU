94943c31d478a1daa1d7e10c322a8e4906e4e40c
GreasyRooster <greasyRooster1@gmail.com>
2025-08-23 16:49:33 -0700

    get vis icon

--- src/api/proj.js
+++ src/api/proj.js
@@ -1,3 +1,8 @@
+import privateIcon from "/icons/PrivateVisIcon.svg";
+import publicIcon from "/icons/PublicVisIcon.svg";
+import unlistedIcon from "/icons/UnlistedVisIcon.svg";
+import vite from "/vite.svg";
+
 const typesToLangMap = {
     "P5js":"Javascript",
     "PythonTurtle":"Python",
@@ -8,4 +13,16 @@ function getLangForType(rawType){
     return typesToLangMap[rawType]??"?";
 }
 
-export {getLangForType};
+const iconFallback = vite;
+const visIconMap = {
+    "Private": privateIcon,
+    "Public": publicIcon,
+    "Unlisted": unlistedIcon,
+    "Invisible": iconFallback,
+}
+
+function getIconForVisibility(rawVisibility){
+    return visIconMap[rawVisibility]??iconFallback;
+}
+
+export {getLangForType,getIconForVisibility};
