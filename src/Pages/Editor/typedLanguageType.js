c9faa9432ad777cb79d16f52d36120edc84c1197
greasyRooster1 <greasyRooster1@gmail.com>
2025-08-28 13:02:12 -0700

    typed methods

--- src/Pages/Editor/typedLanguageType.js
+++ src/Pages/Editor/typedLanguageType.js
@@ -5,11 +5,11 @@ class TypedLanguageType extends EditorType {
         super(projectMetadata);
     }
 
-    save(){
+    getEditorData(){
 
     }
 
-    setupEditorUI() {
+    setEditorData(){
 
     }
 }
