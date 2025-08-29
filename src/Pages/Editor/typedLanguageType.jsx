b7813c5276927b63ef157c3e0ea78c8f0c934c4f
GreasyRooster <greasyRooster1@gmail.com>
2025-08-28 19:57:37 -0700

    monaco mount and get data

--- src/Pages/Editor/typedLanguageType.jsx
+++ src/Pages/Editor/typedLanguageType.jsx
@@ -4,11 +4,21 @@ import CodePanel from "./panels/code/CodePanel.jsx";
 class TypedLanguageType extends EditorType {
     constructor(props) {
         super(props);
-        this.codeEditor = (<CodePanel />);
+        this.codeEditor = (<CodePanel callbacks={{
+            onMount:this.monacoOnMount
+        }}/>);
+        this.editorRef =null;
+    }
+
+    monacoOnMount = (editor, monaco)=>{
+        this.editorRef = editor;
     }
 
     getEditorData(){
-
+        if(this.editorRef===null){
+            throw new Error("Editor has not yet been mounted")
+        }
+        this.editorRef.current.getValue();
     }
 
     setEditorData(){
