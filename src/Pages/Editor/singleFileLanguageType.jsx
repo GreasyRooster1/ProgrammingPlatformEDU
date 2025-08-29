b9838d0cdce549ff143195e2db02fd0b6bc4b26f
GreasyRooster <greasyRooster1@gmail.com>
2025-08-28 20:16:25 -0700

    single file proto

---
+++ src/Pages/Editor/singleFileLanguageType.jsx
@@ -0,0 +1,10 @@
+import EditorType from "./editorType.jsx";
+import TypedLanguageType from "./typedLanguageType.jsx";
+
+class SingleFileLanguageType extends TypedLanguageType {
+    constructor(projectMetadata) {
+        super(projectMetadata);
+    }
+}
+
+export default SingleFileLanguageType;
