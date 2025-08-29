38cd0b15ba449720f7747b26bdce826ad356e7e1
GreasyRooster <greasyRooster1@gmail.com>
2025-08-28 19:30:01 -0700

    just made it a component

--- src/Pages/Editor/editorType.jsx
+++ src/Pages/Editor/editorType.jsx
@@ -5,19 +5,23 @@ import LessonPanel from "./panels/lesson/LessonPanel.jsx";
 import CodePanel from "./panels/code/CodePanel.jsx";
 import OutputPanel from "./panels/output/OutputPanel.jsx";
 import ConsolePanel from "./panels/console/ConsolePanel.jsx";
+import {handle} from "../../api/util.js"
 
-class EditorType {
-    constructor(projectMetadata) {
-        this.projectMetadata = projectMetadata;
+class EditorType extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ...props.projectMetadata
+        }
     }
 
     save(){
         //throw new Error("Save must be implemented.")
         console.log(this);
-        this.projectMetadata.id="asd"
+        this.setState({projectMetadata:{id:"asd"}})
     }
 
-    getEditorUI(){
+    render(){
         return (
             <PanelGroup direction="horizontal">
                 <Panel defaultSize={25} className={styles.stepsPanel}>
@@ -38,7 +42,7 @@ class EditorType {
                         <PanelResizeHandle />
 
                         <Panel defaultSize={60} className={styles.consolePanel} onClick={handle(this.save,this)}>
-                            {this.projectMetadata.id}
+                            {this.state.projectMetadata.id}
                             <ConsolePanel />
                         </Panel>
                     </PanelGroup>
