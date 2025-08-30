a492711d16b2e4a94452252f19b8c8193f6b5e63
GreasyRooster <greasyRooster1@gmail.com>
2025-08-30 10:45:10 -0700

    single file functional too

--- src/Pages/Editor/singleFileLanguageType.jsx
+++ src/Pages/Editor/singleFileLanguageType.jsx
@@ -1,23 +1,29 @@
 import EditorType from "./editorType.jsx";
 import TypedLanguageType from "./typedLanguageType.jsx";
 import {net} from "../../api/net.js";
+import {useState} from "react";
 
-class SingleFileLanguageType extends TypedLanguageType {
-    constructor(projectMetadata) {
-        super(projectMetadata);
-        console.log("load")
-        console.log(this.loadProject);
-    }
+function SingleFileLanguageType(props){
+    const [editorData,setEditorData] = useState("");
 
-    loadProject = ()=> {
+
+    const loadProject = ()=>{
         console.log("Loading project...");
         net.proj.getFile(this.token,[this.state.id,"main"],undefined,this.onRequestError).then((contents)=>{
             contents.text().then((text)=>{
                 console.log(text);
-                this.setEditorData(text);
+                setEditorData(text);
             })
         });
     }
+
+    return (
+        <TypedLanguageType
+            loadProject={loadProject}
+            setEditorData={setEditorData}
+            {...props}
+        />
+    )
 }
 
 export default SingleFileLanguageType;
