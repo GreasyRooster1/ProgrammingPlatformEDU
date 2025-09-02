d97aed657fda5cf9445bf9f4d39b8ade118924c3
GreasyRooster <greasyRooster1@gmail.com>
2025-09-01 17:35:31 -0700

    event links

--- src/Pages/Editor/Executors/P5jsExecutor.jsx
+++ src/Pages/Editor/Executors/P5jsExecutor.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import Executor from "../Executor.jsx";
 
 function P5JsExecutor(props) {
+    const onExecute = () => {
+
+    }
+
+    const onCleanup = () => {
+
+    }
+
     return (
         <Executor
+            onExecute={onExecute}
+            onCleanup={onCleanup}
             {...props}
         >
             <iframe src={}>
