eb99f3c89c4e0dcf5e233f27ee9203229fa2cdb8
GreasyRooster <greasyRooster1@gmail.com>
2025-09-04 17:50:58 -0700

    remove output buttons

--- src/Pages/Editor/panels/output/OutputPanel.jsx
+++ src/Pages/Editor/panels/output/OutputPanel.jsx
@@ -8,22 +8,11 @@ import link from "/icons/ExternalLink.svg"
 
 function OutputPanel(props) {
     return (
-        <HeaderFlex className={styles.wrapper} headClassName={styles.head} bodyClassName={styles.body} headContent={
-            <>
-                <div className={`${styles.controlButtons} ${styles.iconContainers}`}>
-                    <img src={play}/>
-                    <img src={stop}/>
-                </div>
-                <div className={`${styles.popButtons} ${styles.iconContainers}`}>
-                    <img src={link}/>
-                </div>
-            </>
-        }>
-
-        <div className={styles.executorContainer}>
-            {props.executor}
+        <div className={styles.body}>
+            <div className={styles.executorContainer}>
+                {props.executor}
+            </div>
         </div>
-        </HeaderFlex>
     );
 }
 
