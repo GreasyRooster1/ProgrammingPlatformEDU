658e9b41f99a519c33e32c4c6d9d6f85666f67ef
GreasyRooster <greasyRooster1@gmail.com>
2025-09-07 10:56:01 -0700

    lesson page stylesheet

--- src/Pages/Editor/panels/lesson/LessonPanel.jsx
+++ src/Pages/Editor/panels/lesson/LessonPanel.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import styles from "./lessonPanel.module.css";
 
 function LessonPanel() {
     return (
-        <>
-        </>
+        <div className={styles.lesson}>
+
+        </div>
     );
 }
 
