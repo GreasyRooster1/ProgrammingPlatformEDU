524cea6fd0bf22773ddca9013ef70d5513a362cd
GreasyRooster <greasyRooster1@gmail.com>
2025-09-07 10:42:23 -0700

    vault steps

--- src/Pages/Editor/panels/lesson/LessonPanel.jsx
+++ src/Pages/Editor/panels/lesson/LessonPanel.jsx
@@ -5,21 +5,6 @@ import logo from "/vite.svg";
 function LessonPanel() {
     return (
         <>
-            <Step title="Step 1" image={logo} type="info">
-                This is a test step
-            </Step>
-            <Step title="Step 2" image={logo} type="code">
-                This is a test step
-            </Step>
-            <Step title="Step 3" image={logo} type="challenge">
-                This is a test step
-            </Step>
-            <Step title="Step 4" image={logo} type="check">
-                This is a test step
-            </Step>
-            <Step title="Step 5" image={logo} type="next">
-                This is a test step
-            </Step>
         </>
     );
 }
