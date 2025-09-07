c46628e1d6d3ee6ae8261ce988a2273bf31f4568
GreasyRooster <greasyRooster1@gmail.com>
2025-09-07 10:59:20 -0700

    basic lesson content

--- src/Pages/Editor/panels/lesson/LessonPanel.jsx
+++ src/Pages/Editor/panels/lesson/LessonPanel.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from "./lessonPanel.module.css";
+import MedTitle from "../../../../components/text/MedTitle.jsx";
 
 function LessonPanel() {
     return (
         <div className={styles.lesson}>
-
+            <MedTitle className={styles.title}>
+                Title
+            </MedTitle>
+            <div className={styles.textDescription}>
+                test desc
+            </div>
         </div>
     );
 }
