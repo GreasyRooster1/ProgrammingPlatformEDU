806271e32de1c50034574067e8332529ce0770e2
GreasyRooster <greasyRooster1@gmail.com>
2025-09-11 18:10:25 -0700

    auth

--- src/Pages/Editor/SingleFileLanguageType.jsx
+++ src/Pages/Editor/SingleFileLanguageType.jsx
@@ -2,20 +2,22 @@ import EditorType from "./EditorType.jsx";
 import TypedLanguageType from "./TypedLanguageType.jsx";
 import {net} from "../../api/net/net.js";
 import {useState} from "react";
+import {useAuth} from "react-oidc-context";
 
 function SingleFileLanguageType(props){
+    const auth = useAuth();
     const [editorData,setEditorData] = useState("");
 
 
     const loadProject = async ()=>{
         console.log("Loading project...");
-        let blob = await net.proj.getFile(props.token,[props.projectMetadata.id,"main"])
+        let blob = await net.proj.getFile(auth.user?.id_token,[props.projectMetadata.id,"main"])
         let text = await blob.text();
         setEditorData(text)
     }
 
     const onSave = ()=>{
-        net.proj.saveFile(props.token,[props.projectMetadata.id,"main"],editorData,undefined)
+        net.proj.saveFile(auth.user?.id_token,[props.projectMetadata.id,"main"],editorData,undefined)
     }
 
     return (
