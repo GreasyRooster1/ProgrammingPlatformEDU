bca31782acef61230946ec6cb57a16a33e87c4c3
GreasyRooster <greasyRooster1@gmail.com>
2025-09-11 17:17:45 -0700

    change auth provider

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -1,28 +1,28 @@
 import React, {useEffect} from 'react';
 import {useAuth0} from "@auth0/auth0-react";
+import {useAuth} from "react-oidc-context";
+import LoadingScreen from "../LoadingScreen.jsx";
 
 function AuthProvider(props) {
-    const {isAuthenticated,isLoading,getAccessTokenSilently } = useAuth0();
-    let setUserData = props.setUserData;
-    useEffect(() => {
-        if(!isLoading && isAuthenticated) {
-            getAccessTokenSilently().then((e)=>{
-                setUserData({
-                    token:e,
-                    isAuthenticated:true,
-                })
-            })
-        }
-    },[isLoading,isAuthenticated])
+    const auth = useAuth();
+
+    if (auth.isLoading) {
+        return <LoadingScreen />;
+    }
+
+    if (auth.error) {
+        //todo error screen
+        return <div>Encountering error... {auth.error.message}</div>;
+    }
 
     return props.children;
 }
 
-export function defaultAuthData(){
-    return {
-        token:"",
-        isAuthenticated: false,
-    }
+export function signOutRedirect(){
+    const clientId = "3jssdi6ka6iq8s2aeil20f9jg5";
+    const logoutUri = "<logout uri>";
+    const cognitoDomain = "https://<user pool domain>";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 }
 
 export default AuthProvider;
