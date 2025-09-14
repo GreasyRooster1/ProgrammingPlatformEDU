9bdab8f923735341acd4f830816727d6eff3ef31
GreasyRooster <greasyRooster1@gmail.com>
2025-09-13 18:28:29 -0700

    fix some

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -19,8 +19,8 @@ function AuthProvider(props) {
 
 export function signOutRedirect(){
     const clientId = "3jssdi6ka6iq8s2aeil20f9jg5";
-    const logoutUri = "<logout uri>";
-    const cognitoDomain = "https://<user pool domain>";
+    const logoutUri = "http://localhost:5173/";
+    const cognitoDomain = "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_nQhVA9vHC";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { AuthProvider } from "react-oidc-context";
 const cognitoAuthConfig = {
     authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_nQhVA9vHC",
     client_id: "3jssdi6ka6iq8s2aeil20f9jg5",
-    redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+    redirect_uri: "http://localhost:5173/",
     response_type: "code",
     scope: "phone openid email",
 };
