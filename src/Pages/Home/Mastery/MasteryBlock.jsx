42b04b6cc9eb33a582a1d35d3d55622967249c8b
GreasyRooster <greasyRooster1@gmail.com>
2025-09-16 20:03:01 -0700

    generate hexagons

--- src/Pages/Home/Mastery/MasteryBlock.jsx
+++ src/Pages/Home/Mastery/MasteryBlock.jsx
@@ -3,10 +3,15 @@ import styles from "./masteryBlock.module.css"
 import HexSVG from "./HexSVG.jsx";
 
 function MasteryBlock(props) {
+    let hexagons = []
+    let numHex = 4;
+    for(let i=1;i<=numHex;i++){
+        hexagons.push(i*(50/numHex));
+    }
     return (
         <div className={styles.parent}>
             <svg className={styles.svg} viewBox={`-50 -50 100 100`}>
-                <HexSVG r={50}/>
+                {hexagons.map(s => <HexSVG key={s} r={s} />)}
             </svg>
         </div>
     );
