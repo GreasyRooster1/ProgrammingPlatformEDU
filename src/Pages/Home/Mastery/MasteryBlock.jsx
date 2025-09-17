5d9f6b9479de31145eed5b1a44aaec60445cf758
GreasyRooster <greasyRooster1@gmail.com>
2025-09-16 19:51:34 -0700

    svg

--- src/Pages/Home/Mastery/MasteryBlock.jsx
+++ src/Pages/Home/Mastery/MasteryBlock.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import styles from "./masteryBlock.module.css"
 
 function MasteryBlock(props) {
     return (
-        <>
+        <div className={styles.parent}>
+            <svg className={styles.svg} viewBox={`0 0 100 100`}>
 
-        </>
+            </svg>
+        </div>
     );
 }
 
