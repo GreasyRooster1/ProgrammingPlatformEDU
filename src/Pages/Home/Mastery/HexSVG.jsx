4d41b7172738dfd635d32bfd1e737d3285ee88e2
GreasyRooster <greasyRooster1@gmail.com>
2025-09-16 19:57:18 -0700

    fix scale

--- src/Pages/Home/Mastery/HexSVG.jsx
+++ src/Pages/Home/Mastery/HexSVG.jsx
@@ -5,8 +5,8 @@ function HexSvg(props) {
     let points = []
     let ratio = Math.PI*2/6.0
     for(let i=0;i<6;i++){
-        let x = Math.cos(i*ratio)*props.r+props.cx;
-        let y = Math.cos(i*ratio)*props.r+props.cy;
+        let x = Math.cos(i*ratio)*props.r+(props.cx??0);
+        let y = Math.cos(i*ratio)*props.r+(props.cy??0);
         points.push(x+" "+y)
     }
 
--- src/Pages/Home/Mastery/MasteryBlock.jsx
+++ src/Pages/Home/Mastery/MasteryBlock.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from "./masteryBlock.module.css"
+import HexSVG from "./HexSVG.jsx";
 
 function MasteryBlock(props) {
     return (
         <div className={styles.parent}>
-            <svg className={styles.svg} viewBox={`0 0 100 100`}>
-
+            <svg className={styles.svg} viewBox={`-50 -50 100 100`}>
+                <HexSVG r={50}/>
             </svg>
         </div>
     );
