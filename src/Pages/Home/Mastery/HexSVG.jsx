7cbe2c8e526877d5fddea5acea7b885bc2ff3194
GreasyRooster <greasyRooster1@gmail.com>
2025-09-16 20:01:13 -0700

    strokes

--- src/Pages/Home/Mastery/HexSVG.jsx
+++ src/Pages/Home/Mastery/HexSVG.jsx
@@ -6,13 +6,16 @@ function HexSvg(props) {
     let ratio = Math.PI*2/6.0
     for(let i=0;i<6;i++){
         let x = Math.cos(i*ratio)*props.r+(props.cx??0);
-        let y = Math.cos(i*ratio)*props.r+(props.cy??0);
+        let y = Math.sin(i*ratio)*props.r+(props.cy??0);
         points.push(x+" "+y)
     }
 
     return (
         <polygon
             points={points.join(' ')}
+            stroke={"var(--neutral-icon)"}
+            strokeWidth={.2}
+            fill={"transparent"}
             {...props}
         />
     );
