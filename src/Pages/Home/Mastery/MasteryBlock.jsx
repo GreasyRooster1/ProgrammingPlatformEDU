158e15b859507454329462ed7f7b954dbdb36e00
GreasyRooster <greasyRooster1@gmail.com>
2025-09-16 20:15:05 -0700

    color

--- src/Pages/Home/Mastery/MasteryBlock.jsx
+++ src/Pages/Home/Mastery/MasteryBlock.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from "./masteryBlock.module.css"
 import HexSVG from "./HexSVG.jsx";
 import HexStarSVG from "./HexStarSVG.jsx";
+import DataHexSVG from "./DataHexSVG.jsx";
 
 function MasteryBlock(props) {
     let hexagons = []
@@ -14,6 +15,7 @@ function MasteryBlock(props) {
             <svg className={styles.svg} viewBox={`-50 -50 100 100`}>
                 {hexagons.map(s => <HexSVG key={s} r={s} />)}
                 <HexStarSVG r={50}/>
+                <DataHexSVG r={50} data={[1.0,.3,.7,.9,.6,.5]} strokeWidth={1} stroke={"var(--primary)"} fill={"var(--transparent-primary"}/>
             </svg>
         </div>
     );
