1b4ceb3ee3104b3a81b4abb5d508b41c9ad4deb8
GreasyRooster <greasyRooster1@gmail.com>
2025-09-16 20:20:11 -0700

    padding

--- src/Pages/Home/Mastery/MasteryBlock.jsx
+++ src/Pages/Home/Mastery/MasteryBlock.jsx
@@ -3,6 +3,7 @@ import styles from "./masteryBlock.module.css"
 import HexSVG from "./HexSVG.jsx";
 import HexStarSVG from "./HexStarSVG.jsx";
 import DataHexSVG from "./DataHexSVG.jsx";
+import MedTitle from "../../../components/text/MedTitle.jsx";
 
 function MasteryBlock(props) {
     let hexagons = []
@@ -12,11 +13,13 @@ function MasteryBlock(props) {
     }
     return (
         <div className={styles.parent}>
+            <MedTitle className={styles.heading}>Mastery</MedTitle>
             <svg className={styles.svg} viewBox={`-50 -50 100 100`}>
                 {hexagons.map(s => <HexSVG key={s} r={s} />)}
                 <HexStarSVG r={50}/>
                 <DataHexSVG r={50} data={[1.0,.3,.7,.9,.6,.5]} strokeWidth={1} stroke={"var(--primary)"} fill={"var(--transparent-primary"}/>
             </svg>
+            <div />
         </div>
     );
 }
