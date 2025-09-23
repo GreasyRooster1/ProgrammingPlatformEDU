039118d9e4d067620f7f15017902c19d87b797ec
GreasyRooster <greasyRooster1@gmail.com>
2025-09-22 21:00:31 -0700

    select

--- src/Pages/Home/ProjectsBlock/LanguageSelector.jsx
+++ src/Pages/Home/ProjectsBlock/LanguageSelector.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./languageSelector.module.css"
 
 function LanguageSelector(props) {
+    const [selectedLanguage, setSelectedLanguage] = useState(null);
 
+    let clickHandle = (language)=>{
+        setSelectedLanguage(language)
+    }
+    let getSelectedClass = (language) => {
+        if(!selectedLanguage){
+            return "";
+        }
+        return selectedLanguage.name === language.name ? styles.selected : "";
+    }
 
     return (
         <div className={styles.selector}>
             {
                 props.languages.map((language) => (
-                    <div key={language.name} className={styles.item}>
+                    <div key={language.name} className={`${styles.item} ${getSelectedClass(language)}`} onClick={()=>{clickHandle(language)}} >
                         <img src={language.icon} />
                         <span className={styles.itemName}>{language.name}</span>
                     </div>
