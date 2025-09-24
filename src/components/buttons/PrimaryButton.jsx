4b5e8f9506186b471462293c90ff25c4610b567f
GreasyRooster <greasyRooster1@gmail.com>
2025-09-23 20:48:02 -0700

    prevent click if inactive

--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import styles from './primaryButton.module.css'
 
 function PrimaryButton(props) {
+    const clickHandle = ()=>{
+        if(!props.inactive){
+            props.onClick()
+        }
+    }
+
     return (
-        <div className={`${styles.primaryButton} ${props.className??""} ${props.inactive?styles.inactive:""}`} onClick={props.onClick}>
+        <div className={`${styles.primaryButton} ${props.className??""} ${props.inactive?styles.inactive:""}`} onClick={clickHandle}>
             <span>
                 {props.children}
                 {props.icon&& <img className={styles.icon} src={props.icon} alt="*"/>}
