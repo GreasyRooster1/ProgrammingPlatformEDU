ab069cd5ea71787067295a7802bc7775776bf5a2
GreasyRooster <greasyRooster1@gmail.com>
2025-09-24 19:50:12 -0700

    shelf mantine

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,20 @@ import HomePage from "./Pages/Home/HomePage.jsx";
 import EditorPage from "./Pages/Editor/EditorPage.jsx";
 import LandingPage from "./Pages/Landing/LandingPage.jsx";
 import LoadingTestPage from "./Pages/Loading/LoadingTestPage.jsx";
-import '@mantine/core/styles.css';
 
-import { MantineProvider } from '@mantine/core';
+// import { MantineProvider } from '@mantine/core';
 
 function App() {
 
   return (
-      <MantineProvider>
+      // <MantineProvider>
           <Routes>
             <Route path="/home" element={<HomePage />} />
             <Route path="/editor" element={<EditorPage />} />
             <Route path="/" element={<LandingPage />} />
             <Route path="/loadingTest" element={<LoadingTestPage />} />
           </Routes>
-      </MantineProvider>
+      // </MantineProvider>
   )
 }
 
