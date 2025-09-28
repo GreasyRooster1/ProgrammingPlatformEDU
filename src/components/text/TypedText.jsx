3e6569a36758424a4f5a849843a54b12a9cce2cb
GreasyRooster <greasyRooster1@gmail.com>
2025-09-27 17:43:14 -0700

    typed text

---
+++ src/components/text/TypedText.jsx
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+
+function TypedText(props) {
+    const [currentText, setCurrentText] = useState('');
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const delay = props.delay??30;
+
+    useEffect(() => {
+        if(props.disableAnim){
+            return;
+        }
+
+        if (currentIndex < props.text) {
+            const timeout = setTimeout(() => {
+                setCurrentText(prevText => prevText + text[currentIndex]);
+                setCurrentIndex(prevIndex => prevIndex + 1);
+            }, delay);
+
+            return () => clearTimeout(timeout);
+        }
+
+    }, [props.text]);
+
+    return (
+        <span>{currentText}</span>
+    );
+}
+
+export default TypedText;
