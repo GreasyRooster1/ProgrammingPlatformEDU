4556f3589db3600553a9380cb17cba2b3d4112d1
GreasyRooster <greasyRooster1@gmail.com>
2025-09-29 19:49:36 -0700

    load lesson data

--- src/Pages/Editor/editorTypes/LessonType.jsx
+++ src/Pages/Editor/editorTypes/LessonType.jsx
@@ -2,12 +2,23 @@ import SingleFileLanguageType from "../SingleFileLanguageType.jsx";
 import {useRef, useState} from "react";
 import P5Executor from "../Executors/P5Executor.jsx";
 import EditorType from "../editorType.jsx";
+import {net} from "../../../api/net/net.js";
+import {useAuth} from "react-oidc-context";
 
 function LessonType(props){
+    let auth = useAuth();
+    const [lessonJson, setLessonJson] = useState({});
+
+    const loadProject = async ()=>{
+        let blob = await net.proj.getFile(auth.user?.access_token,[props.projectMetadata.id,"main"])
+        let text = await blob.text();
+        setLessonJson(text)
+    }
 
     return (
         <EditorType
             noOutput
+            loadProject={loadProject}
             {...props}
         />
     )
