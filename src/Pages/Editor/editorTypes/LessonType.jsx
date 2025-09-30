593288aa15e407adc3cf72b85a960c075bb68b73
GreasyRooster <greasyRooster1@gmail.com>
2025-09-29 19:05:09 -0700

    lesson type

---
+++ src/Pages/Editor/editorTypes/LessonType.jsx
@@ -0,0 +1,15 @@
+import SingleFileLanguageType from "../SingleFileLanguageType.jsx";
+import {useRef, useState} from "react";
+import P5Executor from "../Executors/P5Executor.jsx";
+import EditorType from "../editorType.jsx";
+
+function LessonType(props){
+
+    return (
+        <EditorType
+            noOutput
+        />
+    )
+}
+
+export default LessonType;
