7ffc0d66a875a7adb2d741a4513476a1955232f0
GreasyRooster <greasyRooster1@gmail.com>
2025-09-30 21:03:09 -0700

    steps looking rough

--- src/Pages/Editor/editorTypes/LessonType/Step.jsx
+++ src/Pages/Editor/editorTypes/LessonType/Step.jsx
@@ -9,8 +9,8 @@ function Step(props) {
     return (
         <div className={`${styles.step} ${props.selected?styles.selected:""}`} onClick={click}>
             <div className={styles.stepHead}>
-                <span className={styles.stepType}>{props.step.type}</span>
                 <span className={styles.id}>{props.stepIndex}</span>
+                <span className={styles.stepType}>{props.step.type}</span>
             </div>
             <span className={styles.stepPreview}>{props.step.components[0]?.text??"No preview"}</span>
         </div>
