e3338be1c8695fb2a13b09ade40c9c0880362339
GreasyRooster <greasyRooster1@gmail.com>
2025-09-30 20:52:41 -0700

    select step

--- src/Pages/Editor/editorTypes/LessonType/Step.jsx
+++ src/Pages/Editor/editorTypes/LessonType/Step.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 import styles from "./stepsTab.module.css"
 
 function Step(props) {
+    let click = ()=>{
+        props.handle(props.stepIndex)
+    }
     return (
-        <div className={styles.step}>
+        <div className={`${styles.step} ${props.selected?styles.selected:""}`} onClick={click}>
             <span className={styles.stepTitle}>{props.step.name}</span>
             <span className={styles.stepType}>{props.step.type}</span>
         </div>
--- src/Pages/Editor/editorTypes/LessonType/StepsTab.jsx
+++ src/Pages/Editor/editorTypes/LessonType/StepsTab.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './stepsTab.module.css';
 import Step from "./Step.jsx";
 
 function StepsTab(props) {
+    const [selectedStep, setSelectedStep] = useState(null);
+    let clickHandle = (index)=>{
+        setSelectedStep(index);
+    }
+
     return (
         <div className={styles.body}>
             <span className={styles.title}>Steps ({props.lessonData.steps.length})</span>
             <div className={styles.stepGrid}>
                 {
                     props.lessonData.steps.map((step,index) => (
-                        <Step key={index} step={step}/>
+                        <Step key={index} step={step} stepIndex={index} selected={selectedStep===index} handle={clickHandle} />
                     ))
                 }
             </div>
