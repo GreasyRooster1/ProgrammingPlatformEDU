61a73c76d19574d3c1b06d41bbba1a712e8fbb7d
GreasyRooster <greasyRooster1@gmail.com>
2025-10-01 20:35:13 -0700

    title

---
+++ src/Pages/Editor/panels/lesson/TitleComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MedTitle from "../../../../components/text/MedTitle.jsx";
+
+function TitleComponent(props) {
+    return (
+        <MedTitle>{props.component.text}</MedTitle>
+    );
+}
+
+export default TitleComponent;
