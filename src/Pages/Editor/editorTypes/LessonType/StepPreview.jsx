62c6f05842ba09155b78b5feb37684c20be235a9
GreasyRooster <greasyRooster1@gmail.com>
2025-10-01 21:03:54 -0700

    preview prompt

--- src/Pages/Editor/editorTypes/LessonType/StepPreview.jsx
+++ src/Pages/Editor/editorTypes/LessonType/StepPreview.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import style from './stepPreview.module.css'
+import styles from './stepPreview.module.css'
 import StepComponent from "../../panels/lesson/StepComponent.jsx";
+import Loading from "../../../../Loading.jsx";
+import MedTitle from "../../../../components/text/MedTitle.jsx";
 
 function StepPreview(props) {
+
+    if(!props.stepData){
+        return (
+            <div className={styles.selectPrompt}>
+                <MedTitle>Select A Step...</MedTitle>
+            </div>
+        )
+    }
+
     return (
         <>
         {
