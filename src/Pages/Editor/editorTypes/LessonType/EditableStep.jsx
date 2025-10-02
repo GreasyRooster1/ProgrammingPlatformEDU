a1407eb98dcc9355fa2ea4b0ba853a0f8cd9b1e7
GreasyRooster <greasyRooster1@gmail.com>
2025-10-01 21:23:10 -0700

    editable step

---
+++ src/Pages/Editor/editorTypes/LessonType/EditableStep.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./editableStep.module.css"
+
+function EditableStep(props) {
+    return (
+        <div className={styles.editableStep}>
+
+        </div>
+    );
+}
+
+export default EditableStep;
