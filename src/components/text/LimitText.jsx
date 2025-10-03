5d19f49347086a9d561239b7a699fe5363ddda2e
GreasyRooster <greasyRooster1@gmail.com>
2025-10-02 18:51:02 -0700

    limitText

---
+++ src/components/text/LimitText.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+
+function LimitText(props) {
+    const [text, setText] = useState('');
+
+    useEffect(() => {
+        if (typeof props.children !== 'string' && typeof props.children !== 'number') {
+            setText("Cannot render!");
+            return;
+        }
+        let str = props.children.toString();
+        if(str.length<=props.maxLength){
+            return str.substring(0,props.maxLength-2)+"...";
+        }
+        return str;
+    }, [props.children,props.maxLength]);
+
+    return (
+        <span className={`${props.className}`}>
+            {text}
+        </span>
+    );
+}
+
+export default LimitText;
