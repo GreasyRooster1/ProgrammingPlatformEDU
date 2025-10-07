82e308c6dec63d9f6f46ada1631b13ccc841d667
GreasyRooster <greasyRooster1@gmail.com>
2025-10-07 16:59:31 -0700

    nuke

--- src/Pages/Editor/editorTypes/LessonType/StepPreview.jsx
+++ src/Pages/Editor/editorTypes/LessonType/StepPreview.jsx
@@ -6,34 +6,36 @@ import MedTitle from "../../../../components/text/MedTitle.jsx";
 import EditableComponent from "./EditableComponent.jsx";
 
 function StepPreview(props) {
-    const [selectedComponent, setSelectedComponent] = useState(null);
-
-    if(!props.stepData){
-        return (
-            <div className={styles.selectPrompt}>
-                <MedTitle>Select A Step...</MedTitle>
-            </div>
-        )
-    }
-
-    const clickHandle = (index)=>{
-        setSelectedComponent(index);
-    }
-
-    return (
-        <div className={styles.preview}>
-        {
-            props.stepData.components.map((component, index) => (
-                <div className={styles.component} onClick={()=>{clickHandle(index)}}>
-                    {selectedComponent===index?
-                        <StepComponent key={index} component={component}/>:
-                        <EditableComponent key={index} component={component}/>
-                    }
-                </div>
-            ))
-        }
-        </div>
-    );
+    // const [selectedComponent, setSelectedComponent] = useState(null);
+    //
+    // if(!props.stepData){
+    //     return (
+    //         <div className={styles.selectPrompt}>
+    //             <MedTitle>Select A Step...</MedTitle>
+    //         </div>
+    //     )
+    // }
+    //
+    // const clickHandle = (index)=>{
+    //     setSelectedComponent(index);
+    // }
+    //
+    //
+    //
+    // return (
+    //     <div className={styles.preview} >
+    //     {
+    //         props.stepData.components.map((component, index) => (
+    //             <div className={styles.component} onClick={()=>{clickHandle(index)}}>
+    //                 {selectedComponent===index?
+    //                     <StepComponent key={index} component={component}/>:
+    //                     <EditableComponent key={index} component={component}/>
+    //                 }
+    //             </div>
+    //         ))
+    //     }
+    //     </div>
+    // );
 }
 
 export default StepPreview;
