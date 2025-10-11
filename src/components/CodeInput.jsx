f6ed70312e0340eba63fc5a1944bdaaf0d4b7861
GreasyRooster <greasyRooster1@gmail.com>
2025-10-10 19:15:38 -0700

    huge changes

--- src/components/CodeInput.jsx
+++ src/components/CodeInput.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Editor} from "@monaco-editor/react";
 
 function CodeInput(props) {
-    const  handleEditorChange = (value, event) => {
+    const handleEditorChange = (value, event) => {
         props.setValue(value);
     }
 
