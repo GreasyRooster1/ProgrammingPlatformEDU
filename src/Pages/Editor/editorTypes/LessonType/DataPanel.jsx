08128b67a14bb601ef9e410433601aba755b5ec1
GreasyRooster <greasyRooster1@gmail.com>
2025-10-10 19:38:36 -0700

    data panel

--- src/Pages/Editor/editorTypes/LessonType/DataPanel.jsx
+++ src/Pages/Editor/editorTypes/LessonType/DataPanel.jsx
@@ -1,13 +1,17 @@
 import React, {useState} from 'react';
 import CodeInput from "../../../../components/CodeInput.jsx";
+import TitleDesc from "../../../../components/text/TitleDesc.jsx";
+
+import styles from "./dataPanel.module.css"
 
 function DataPanel(props) {
     const [starterCode,setStarterCode] = useState();
 
     return (
-        <>
+        <div className={styles.dataPanel}>
+            <TitleDesc title={"Boilerplate Code"}>Add some code that will run with this project</TitleDesc>
             <CodeInput value={starterCode} setValue={setStarterCode} language={"javascript"} />
-        </>
+        </div>
     );
 }
 
