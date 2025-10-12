dbf3da608b6677958496bfcaa5dddb66c534f821
GreasyRooster <greasyRooster1@gmail.com>
2025-10-11 17:33:04 -0700

    name

--- src/Pages/Editor/editorTypes/LessonType/DataPanel.jsx
+++ src/Pages/Editor/editorTypes/LessonType/DataPanel.jsx
@@ -3,14 +3,25 @@ import CodeInput from "../../../../components/CodeInput.jsx";
 import TitleDesc from "../../../../components/text/TitleDesc.jsx";
 
 import styles from "./dataPanel.module.css"
+import SmallTitle from "../../../../components/text/SmallTitle.jsx";
+import TextBox from "../../../../components/Form/TextBox.jsx";
 
 function DataPanel(props) {
+    const [name,setName] = useState("");
     const [starterCode,setStarterCode] = useState("");
+
+    const validateName = (name) => {
+        return name.length > 0 && name.length <= 64;
+    }
 
     return (
         <div className={styles.dataPanel}>
+            <TextBox placeholder={"Lesson name"} value={name} validate={validateName} setValue={setName}></TextBox>
+            <br/>
+
             <TitleDesc title={"Boilerplate Code"}>Add some code that will run with this project</TitleDesc>
             <CodeInput value={starterCode} setValue={setStarterCode} language={"javascript"} />
+            <br/>
         </div>
     );
 }
