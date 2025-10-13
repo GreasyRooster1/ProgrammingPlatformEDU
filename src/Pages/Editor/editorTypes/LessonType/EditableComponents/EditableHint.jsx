50cd152b08d5ccd9f37ef0603c30baf8d6e36a17
GreasyRooster <greasyRooster1@gmail.com>
2025-10-12 17:05:46 -0700

    title

--- src/Pages/Editor/editorTypes/LessonType/EditableComponents/EditableHint.jsx
+++ src/Pages/Editor/editorTypes/LessonType/EditableComponents/EditableHint.jsx
@@ -2,7 +2,8 @@ import React, {useEffect, useState} from 'react';
 import TextBox from "../../../../../components/form/TextBox.jsx";
 
 function EditableText(props) {
-    const [text, setText] = useState(props.component.text);
+    const [text, setText] = useState(props.component.text??"");
+    const [title, setTitle] = useState(props.component.title??"");
 
     useEffect(() => {
         props.setComponentData({
@@ -10,14 +11,22 @@ function EditableText(props) {
             text: text,
         })
     },[text])
+    useEffect(() => {
+        props.setComponentData({
+            ...props.component,
+            title: title,
+        })
+    },[title])
 
     useEffect(() => {
-        setText(props.component.text);
+        setText(props.component.text??"");
+        setTitle(props.component.title??"");
     },[props.stepData]);
 
 
     return (
         <>
+            <TextBox placeholder={"type some content..."} value={title} setValue={setTitle}></TextBox>
             <TextBox placeholder={"type some content..."} value={text} setValue={setText}></TextBox>
         </>
     );
