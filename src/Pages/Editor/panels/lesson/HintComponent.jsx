d22c529974d572b8e4ec50cdf22799bc14b16b63
GreasyRooster <greasyRooster1@gmail.com>
2025-10-16 18:06:59 -0700

    hint component

--- src/Pages/Editor/panels/lesson/HintComponent.jsx
+++ src/Pages/Editor/panels/lesson/HintComponent.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MedTitle from "../../../../components/text/MedTitle.jsx";
 import BlockQuote from "../../../../components/text/BlockQuote.jsx";
+import SmallTitle from "../../../../components/text/SmallTitle.jsx";
+import DropdownIcon from "../../../../icons/DropdownIcon.svg?react"
 
 function HintComponent(props) {
+    const [open, setOpen] = useState();
+
     return (
-        <BlockQuote>{props.component.text}</BlockQuote>
+        <BlockQuote>
+            <div onClick={()=>{setOpen(!open)}}>
+                {props.component.title}
+                <DropdownIcon />
+            </div>
+            {open && <span>{props.component.text}</span>}
+        </BlockQuote>
     );
 }
 
