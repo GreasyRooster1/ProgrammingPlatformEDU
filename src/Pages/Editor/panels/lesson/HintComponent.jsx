1e742cf936e5b9d2f7cfc8be115294851971da1f
GreasyRooster <greasyRooster1@gmail.com>
2025-10-16 18:11:31 -0700

    pass default open

--- src/Pages/Editor/panels/lesson/HintComponent.jsx
+++ src/Pages/Editor/panels/lesson/HintComponent.jsx
@@ -7,7 +7,7 @@ import DropdownIcon from "../../../../icons/DropdownIcon.svg?react"
 import styles from "./stepComponent.module.css";
 
 function HintComponent(props) {
-    const [open, setOpen] = useState();
+    const [open, setOpen] = useState(props.open??false);
 
     return (
         <BlockQuote>
