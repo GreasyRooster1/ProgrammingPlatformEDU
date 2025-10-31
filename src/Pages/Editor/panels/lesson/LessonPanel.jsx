a22b7ddb3f952b01e7a5db4cec14329a7b550607
GreasyRooster <greasyRooster1@gmail.com>
2025-10-30 18:55:15 -0700

    render step

--- src/Pages/Editor/panels/lesson/LessonPanel.jsx
+++ src/Pages/Editor/panels/lesson/LessonPanel.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./lessonPanel.module.css";
 import MedTitle from "../../../../components/text/MedTitle.jsx";
 import CodePanel from "../code/CodePanel.jsx";
 import CodeBlock from "../../../../components/CodeBlock.jsx";
 import Markdown from "../../../../components/Markdown.jsx";
+import StepComponent from "./StepComponent.jsx";
 
 function LessonPanel(props) {
+    const [currentStep, setCurrentStep] = useState(props.savedStep);
 
     return (
         <div className={styles.lesson}>
+            {
+                props.lessonData.steps[currentStep-1].components.map((component, index) => (
+                    <StepComponent key={index} component={component}/>
+                ))
+            }
         </div>
     );
 }
