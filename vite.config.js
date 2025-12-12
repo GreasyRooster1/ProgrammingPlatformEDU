7dc5ce0856ba40be0513f0d9f38bdfe69c08bd24
GreasyRooster <greasyRooster1@gmail.com>
2025-12-11 18:41:44 -0800

    aliases

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,11 @@ export default defineConfig({
     resolve: {
         alias: {
             './runtimeConfig': './runtimeConfig.browser',
+            '@': path.resolve(__dirname, './src'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@api': path.resolve(__dirname, './src/api'),
+            '@icons': path.resolve(__dirname, './src/icons'),
         },
     }
 })
