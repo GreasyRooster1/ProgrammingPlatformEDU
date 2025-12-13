c1041d8a9715d08409342f51a548ad7a7e2bb75d
GreasyRooster <greasyRooster1@gmail.com>
2025-12-12 23:10:54 -0800

    forgot the main thing

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://127.0.0.1:8000";
+export const API_URL = import.meta.env.DEV?"http://localhost:8000":"https://api.bytecodeonline.com";
 export const EXEC_URL = import.meta.env.DEV?"http://localhost:8001":"https://api.bytecodeonline.com";
 export const IS_EXEC_DEV = import.meta.env.DEV;
