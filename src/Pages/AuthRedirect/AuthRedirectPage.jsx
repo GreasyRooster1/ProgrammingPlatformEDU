f787d45a11f2f4926c4fd0f0cebc88c325042e10
GreasyRooster <greasyRooster1@gmail.com>
2025-12-26 14:00:39 -0800

    new page

---
+++ src/Pages/AuthRedirect/AuthRedirectPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScreenPage from "~components/screenPage/ScreenPage.jsx";
+
+function AuthRedirectPage(props) {
+    return (
+        <ScreenPage>
+            Logging in...
+        </ScreenPage>
+    );
+}
+
+export default AuthRedirectPage;
